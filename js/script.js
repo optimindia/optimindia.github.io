0a461758b4351379503adcce6b473ee8838c1e68
optimindia <optimindiapro@gmail.com>
2025-07-04 18:25:37 -0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,61 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const themeToggle = document.getElementById('theme-toggle');
+    const themeIcon = document.getElementById('theme-icon');
+    const siteLogo = document.getElementById('site-logo');
+    const currentTheme = localStorage.getItem('theme');
+
+    if (currentTheme) {
+        document.body.classList.add(currentTheme);
+        if (currentTheme === 'light-mode') {
+            themeIcon.textContent = '🌙';
+            siteLogo.src = 'negro.png'; // Set light mode logo
+        } else {
+            themeIcon.textContent = '☀️';
+            siteLogo.src = 'blanco.png'; // Set dark mode logo
+        }
+    }
+
+    themeToggle.addEventListener('click', () => {
+        document.body.classList.toggle('light-mode');
+        let theme = 'dark-mode';
+        if (document.body.classList.contains('light-mode')) {
+            theme = 'light-mode';
+            themeIcon.textContent = '🌙';
+            siteLogo.src = 'negro.png'; // Change to light mode logo
+        } else {
+            themeIcon.textContent = '☀️';
+            siteLogo.src = 'blanco.png'; // Change to dark mode logo
+        }
+        localStorage.setItem('theme', theme);
+    });
+
+    // Smart navigation bar logic
+    const header = document.querySelector('header');
+    let lastScrollTop = 0;
+    const scrollThreshold = 100; // Define el umbral de desplazamiento en píxeles
+
+    window.addEventListener('scroll', () => {
+        let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+        if (scrollTop > lastScrollTop && scrollTop > scrollThreshold) {
+            // Scrolling down and past the threshold
+            header.classList.add('header-hidden');
+        } else if (scrollTop < lastScrollTop || scrollTop <= scrollThreshold) {
+            // Scrolling up or at the top of the page
+            header.classList.remove('header-hidden');
+        }
+        lastScrollTop = scrollTop;
+    });
+
+    // Smooth scroll for logo click
+    const logoLink = document.querySelector('header .header-logo').closest('a');
+    if (logoLink) {
+        logoLink.addEventListener('click', (e) => {
+            e.preventDefault(); // Prevent default link behavior
+            window.scrollTo({
+                top: 0,
+                behavior: 'smooth'
+            });
+        });
+    }
+});
